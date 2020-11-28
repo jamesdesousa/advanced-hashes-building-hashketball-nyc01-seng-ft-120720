14f0217724e678b001e2957dc0a915e44dc2a356
James DeSousa <james.desousa45@gmail.com>
2020-11-28 22:06:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,6 +19,8 @@ dd = { :player_name => "DeSagna Diop", :number => 2, :shoe => 14, :points => 24,
 
 bg = { :player_name => "Ben Gordon", :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0 }
 
+kw = { :player_name => "Kemba Walker", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12 }
+
 
 
 
@@ -27,24 +29,20 @@ bg = { :player_name => "Ben Gordon", :number => 8, :shoe => 15, :points => 33, :
  
  
  
- hash = {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], 
+ hash = {
+        :home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], 
                    :players => [aa,
                                 re,
                                 bl,
                                 mp,
                                 jt ]},
         :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],
-                  :players => [
-                    
-                    
-                    
-                    
-                    ]}
- 
- 
-   
-   
- }   
+                  :players => [ja,
+                               bb,
+                               dd,
+                               bg,
+                               kw]}
+}   
  
                   
                   
