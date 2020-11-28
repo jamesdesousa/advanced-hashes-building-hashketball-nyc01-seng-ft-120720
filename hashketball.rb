b4644fce51ea5ec64641d7b76df4fdea60de4561
James DeSousa <james.desousa45@gmail.com>
2020-11-28 22:00:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -24,7 +24,7 @@ jt = { :player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19,
                                 jt ]
  }
  },
-        :away => {}}
+        :away => {}
  
    
  
